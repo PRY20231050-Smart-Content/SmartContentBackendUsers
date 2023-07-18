0d15bd7da898b9b09ab099a6269deaf1d3301eef
brigittemendez1302 <u201914955@upc.edu.pe>
2023-07-18 00:08:40 -0500

    add: mysql connection

--- SmartContentBackendUsers/settings.py
+++ SmartContentBackendUsers/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'SmartContentBackendUsers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -119,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
+# Default primary key field typedjango-admin --version
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
