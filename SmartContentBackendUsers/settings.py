dc14e9120e8f185db73272b421239b22307f1fce
brigittemendez1302 <u201914955@upc.edu.pe>
2023-09-10 16:06:32 -0500

    change: charset to database

--- SmartContentBackendUsers/settings.py
+++ SmartContentBackendUsers/settings.py
@@ -102,6 +102,10 @@ DATABASES = {
         'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+        
     }
 }
 
