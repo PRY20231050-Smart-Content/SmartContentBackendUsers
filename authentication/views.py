f06d8141ae644ea44b93cd453fbbf965d38795e7
Carolina <carolina.villegas@alarcongroup.us>
2023-09-25 00:16:43 -0500

    add: try

--- authentication/views.py
+++ authentication/views.py
@@ -34,7 +34,7 @@ class SignupView(APIView):
             # ID token is valid. Get the user's Google Account ID from the decoded token.
             email = idinfo['email']
             
-            print('carrrrrrrrrrrrrasrssdas ', email)
+            print('carrrrrrrrrrrrraassrssdas ', email)
 
             print('idinfo ', idinfo)
 
